b707945546e972e27d5c86ea1f3fe555ee198718
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-01-10 12:30:03 +0000

    commit

---
+++ src/components/product-card.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { FaPlus } from "react-icons/fa";
+
+interface ProductCardProps {
+    productId: string;
+    photo: string;
+    name: string;
+    price: string;
+    stock: string;
+    handler: () => void;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({
+    productId,
+    photo,
+    name,
+    price,
+    stock,
+    handler
+}) => {
+  return (
+    <div className="product-card">
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>$ {price}</span>
+      <div>
+        <button onClick={() => handler() }>
+            <FaPlus />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
 
 const Home = () => {
+  const addToCardhandler = () => {
+
+  }
   return (
     <div className="home">
       <section></section>
@@ -8,7 +12,16 @@ const Home = () => {
         Latest Products
         <Link to="/search" className="findmore">More</Link>
       </h1>
-      <main></main>
+      <main>
+        <ProductCard 
+        photo="src/assets/images/camera.jpeg"
+        name="camera"
+        productId="1"
+        stock="3"
+        price="230"
+        handler={addToCardhandler}
+        />
+      </main>
     </div>
   )
 }
