b911925cdc3b7fe8a55fb26b173e208265b07b37
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-01-11 10:14:44 +0000

    commit

---
+++ ecommerce-backend/src/models/user.ts
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+interface IUser extends Document {
+    _id: string;
+    name: string;
+    photo: string;
+    email: string;
+    role: "admin" | "user";
+    gender: "male" | "female";
+    dob: Date;
+    createdAt: Date;
+    updatedAt: Date;
+    age: number | any;
+}
+
+const schema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            required: [true, "Please enter your id"],
+        },
+        name: {
+            type: String,
+            required: [true, "Please enter your name"],
+        },
+        email: {
+            type: String,
+            unique: [true, "Email already exist"],
+            required: [true, "Please enter your email"],
+            validate: validator.default.isEmail,
+        },
+        photo: {
+            type: String,
+            required: [true, "Please add photo"],
+        },
+        role: {
+            type: String,
+            enum: ["admin", "user"],
+            default: "user",
+        },
+        gender: {
+            type: String,
+            enum: ["male", "female"],
+            required: [true, "Please enter your gender"],
+        },
+        dob: {
+            type: Date,
+            required: [true, "Please enter your date of birth"],
+        },
+    },
+    {
+        timestamps: true,
+    }
+    );
+
+    schema.virtual("age").get(function(){
+        const today = new Date();
+        const dob = this.dob;
+        let age = today.getFullYear() - dob.getFullYear();
+
+        if(today.getMonth() < dob.getMonth() || 
+            today.getMonth() === dob.getMonth() &&
+            today.getDate() < dob.getDate()
+         ){ age--; }
+         return age;
+
+    });
+
+    export const User = mongoose.model<IUser>("User", schema);
